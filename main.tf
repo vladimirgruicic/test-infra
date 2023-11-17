30f48afdadd97afe8c3906e919c497bfd587edb8
vgruicic <s-vgruicic@aras.com>
2023-11-17 16:11:18 +0100

    ashah

--- main.tf
+++ main.tf
@@ -290,7 +290,7 @@ resource "aws_elasticache_cluster" "redis_cluster" {
   cluster_id               = "my-redis-cluster"
   engine                   = "redis"
   engine_version           = "5.0.6"
-  node_type                = "cache.t2.micro"
+  node_type                = "cache.t3.micro"
   num_cache_nodes          = 1
   subnet_group_name        = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids       = [aws_security_group.redis_security_group.id]
