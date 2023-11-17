5f23a462460fa832cb1af25812514bacb179d64b
vgruicic <s-vgruicic@aras.com>
2023-11-17 21:43:55 +0100

    ashah

--- main.tf
+++ main.tf
@@ -322,3 +322,62 @@ resource "aws_s3_bucket" "test_vg_frontend_bucket" {
 #   name       = "redis-subnet-group"
 #   subnet_ids = [aws_subnet.private_subnet.id]
 # }
+
+# Application Load Balancer
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
+
+  enable_deletion_protection = false
+
+  enable_cross_zone_load_balancing = true
+
+  enable_http2 = true
+}
+
+# Target Group for ECS
+resource "aws_lb_target_group" "ecs_target_group" {
+  name     = "ecs-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+
+  health_check {
+    path     = "/"
+    protocol = "HTTP"
+  }
+}
+
+# Listener for ALB
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+  }
+}
+
+# Security Group for ALB
+resource "aws_security_group" "alb_security_group" {
+  name        = "alb-security-group"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
