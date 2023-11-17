b89b127fd185d206162450f41f88f9f3b79d6952
vgruicic <s-vgruicic@aras.com>
2023-11-17 13:45:19 +0100

    ashah

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1"
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet"
@@ -28,7 +28,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-north-1"
+  availability_zone       = "eu-north-1b"
   map_public_ip_on_launch = false
   tags = {
     Name = "PrivateSubnet"
