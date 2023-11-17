01545b18ddba5b41c85a6898d8ae9d01c5e39934
vgruicic <s-vgruicic@aras.com>
2023-11-17 15:17:50 +0100

    ashah

--- main.tf
+++ main.tf
@@ -244,3 +244,48 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "db-subnet-group"
   subnet_ids  = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
 }
+
+# S3 Bucket for Frontend
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "my-frontend-bucket"
+  acl    = "private"  # Adjust based on your access requirements
+
+  versioning {
+    enabled = true
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "your-log-bucket"
+    target_prefix = "s3-logs/"
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "ONEZONE_IA"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+
+  tags = {
+    Name = "FrontendBucket"
+  }
+}
--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -7,11 +7,41 @@ provider "aws" {
 # S3 Bucket for Frontend
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = "my-frontend-bucket"
-  acl    = "public-read"  # Set ACL as needed
+  acl    = "private"  # Adjust based on your access requirements
+
+  versioning {
+    enabled = true
+  }
 
   website {
     index_document = "index.html"
     error_document = "error.html"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "your-log-bucket"
+    target_prefix = "s3-logs/"
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "ONEZONE_IA"
+    }
+
+    expiration {
+      days = 365
+    }
   }
 
   tags = {
