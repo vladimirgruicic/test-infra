ae7343be286a6495fb26693f3633aabd81a77dbe
vgruicic <s-vgruicic@aras.com>
2023-11-17 21:39:35 +0100

    ashah

--- main.tf
+++ main.tf
@@ -285,40 +285,40 @@ resource "aws_s3_bucket" "test_vg_frontend_bucket" {
   }
 }
 
-# ElastiCache Redis Cluster
-resource "aws_elasticache_cluster" "redis_cluster" {
-  cluster_id               = "my-redis-cluster"
-  engine                   = "redis"
-  engine_version           = "5.0.6"
-  node_type                = "cache.t3.micro"
-  num_cache_nodes          = 1
-  subnet_group_name        = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids       = [aws_security_group.redis_security_group.id]
-  parameter_group_name     = "default.redis5.0"
-  port                     = 6379
-}
+# # ElastiCache Redis Cluster
+# resource "aws_elasticache_cluster" "redis_cluster" {
+#   cluster_id               = "my-redis-cluster"
+#   engine                   = "redis"
+#   engine_version           = "5.0.6"
+#   node_type                = "cache.t3.micro"
+#   num_cache_nodes          = 1
+#   subnet_group_name        = aws_elasticache_subnet_group.redis_subnet_group.name
+#   security_group_ids       = [aws_security_group.redis_security_group.id]
+#   parameter_group_name     = "default.redis5.0"
+#   port                     = 6379
+# }
 
-# Security Group for Redis
-resource "aws_security_group" "redis_security_group" {
-  name        = "redis-security-group"
-  description = "Security group for ElastiCache Redis"
-  vpc_id      = aws_vpc.my_vpc.id
-  ingress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# # Security Group for Redis
+# resource "aws_security_group" "redis_security_group" {
+#   name        = "redis-security-group"
+#   description = "Security group for ElastiCache Redis"
+#   vpc_id      = aws_vpc.my_vpc.id
+#   ingress {
+#     from_port   = 6379
+#     to_port     = 6379
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-# ElastiCache Subnet Group
-resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "redis-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet.id]
-}
+# # ElastiCache Subnet Group
+# resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+#   name       = "redis-subnet-group"
+#   subnet_ids = [aws_subnet.private_subnet.id]
+# }
