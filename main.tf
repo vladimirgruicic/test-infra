18041a741b1016a94c75f6485939d1657e591534
vgruicic <s-vgruicic@aras.com>
2023-11-17 16:18:06 +0100

    ashah

--- main.tf
+++ main.tf
@@ -294,7 +294,7 @@ resource "aws_elasticache_cluster" "redis_cluster" {
   num_cache_nodes          = 1
   subnet_group_name        = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids       = [aws_security_group.redis_security_group.id]
-  parameter_group_name     = "default.redis3.2"
+  parameter_group_name     = "default.redis5.0"
   port                     = 6379
 }
 
