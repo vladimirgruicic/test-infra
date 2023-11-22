a8d511e1004ec3c2b03ffdd77b738c1f0513bdc2
vladimirgruicic <55236547+vladimirgruicic@users.noreply.github.com>
2023-11-22 10:38:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -557,3 +557,21 @@ resource "aws_cloudfront_distribution" "test_vg_frontend_distribution" {
     cloudfront_default_certificate = true
   }
 }
+
+# Route 53 Domain
+resource "aws_route53_zone" "my_domain" {
+  name = "your_domain_name"
+}
+
+# DNS Record for CloudFront
+resource "aws_route53_record" "cloudfront_dns" {
+  zone_id = aws_route53_zone.my_domain.zone_id
+  name    = "testdomain.com"
+  type    = "CNAME"
+
+  alias {
+    name                   = aws_cloudfront_distribution.my_cloudfront.domain_name
+    zone_id               = aws_cloudfront_distribution.my_cloudfront.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
